7498aa95ae2c4c7fa73717de99b0b6b7dc0d9095
Andrew Monks <a@monks.co>
2015-05-04 14:36:37 -0500

    fix urls

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,8 +38,8 @@ module.exports = function(grunt) {
         },
         files: [
           { "build/index.html": "src/jade/index.jade" },
-          { "build/info/rules.html": "src/jade/rules.jade" },
-          { "build/info/thoughts.html": "src/jade/thoughts.jade" }
+          { "build/rules.html": "src/jade/rules.jade" },
+          { "build/thoughts.html": "src/jade/thoughts.jade" }
         ]
       }
     },
